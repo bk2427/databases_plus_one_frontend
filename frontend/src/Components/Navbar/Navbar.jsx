f1e79b906295a4d008e7ef8141a7b3afbcf83ad2
bk2427 <71089069+bk2427@users.noreply.github.com>
2024-04-01 21:57:21 -0400

    simplify nav bar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PAGES = [
-    { label: 'Home', destination: '/home' },
+    { label: 'Home', destination: '/' },
     { label: 'Users', destination: '/Users' },
     { label: 'Restaurants', destination: '/Restaurants' },
     { label: 'User Menu', destination: '/MenuData' },
-    
 ];
 
 function Navbar() {
 
     const mapper = (page) => (
-        <li key={page.destination} className={window.location.pathname === page.destination ? 'active' : ''}>
-          <Link to={page.destination}>
-            {page.label}
-          </Link>
+        <li>
+            <Link to={page.destination}>
+                {page.label}
+            </Link>
         </li>
     );
 
