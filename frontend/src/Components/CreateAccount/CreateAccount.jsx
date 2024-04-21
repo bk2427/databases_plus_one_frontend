72a4bf9700216994868826d09b83a65dba671f0c
Carolina Martin <carolinamartin@10-21-178-141.dynapool.wireless.nyu.edu>
2024-04-21 18:19:16 -0400

    add CSS to Create Account page

--- frontend/src/Components/CreateAccount/CreateAccount.jsx
+++ frontend/src/Components/CreateAccount/CreateAccount.jsx
@@ -1,8 +1,6 @@
-
 // import { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 // import axios from 'axios';
-
 
 // import { BACKEND_URL } from '../../constants';
 
@@ -15,13 +13,10 @@
 // 	const [password, setPassword] = useState('');
 //     const [userIds, setUserIds] = useState([]);
 
-
 // 	const changeFirstName = (event) => { setFirstName(event.target.value); };
 // 	const changeLastName  = (event) => {  setLastName(event.target.value); };
 // 	const changeEmail  = (event) => {  setEmail(event.target.value); };
 // 	const changePassword  = (event) => {  setPassword(event.target.value); };
-  
-
 
 // 	const addUser = (event) => {
 // 		event.preventDefault();
@@ -62,11 +57,10 @@
 // 			<input type="text" id="password" value={password} onChange={changePassword} />
 
 // 			<button type="submit" onClick={addUser}>Create Account</button>
-            
+
 // 		</form>
 // 	);
 // }
-
 
 // function CreateAccount() {
 //     const [error, setError] = useState('');
@@ -79,7 +73,7 @@
 //             const usersObject = response.data.DATA;
 //             const usersArray = Object.values(usersObject);
 //             setUsers(usersArray);
-            
+
 //         })
 //         .catch((error) => {
 //             if (error.response && error.response.data && error.response.data.message) {
@@ -93,8 +87,6 @@
 //     useEffect(() => {
 //         fetchUsers();
 //     }, []);
-
-
 
 //     return (
 //          <div className="wrapper">
@@ -112,102 +104,160 @@
 
 // export default CreateAccount;
 
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./CreateAccount.css";
 
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
-import { BACKEND_URL } from '../../constants';
+import { BACKEND_URL } from "../../constants";
 
 const USERS_ENDPOINT = `${BACKEND_URL}/users`;
 
 function CreateAccount() {
-    const [error, setError] = useState('');
-    const [users, setUsers] = useState([]); // Define the users state
-    const [successMessage, setSuccessMessage] = useState('');
+  const [error, setError] = useState("");
+  const [users, setUsers] = useState([]); // Define the users state
+  const [successMessage, setSuccessMessage] = useState("");
 
-    useEffect(() => {
-        fetchUsers();
-    }, []);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-    const fetchUsers = () => {
-        axios.get(USERS_ENDPOINT)
-            .then((response) => {
-                const usersObject = response.data.DATA;
-                const usersArray = Object.values(usersObject);
-                setUsers(usersArray);
-            })
-            .catch((error) => {
-                if (error.response && error.response.data && error.response.data.message) {
-                    setError(error.response.data.message);
-                } else {
-                    setError('There was a problem fetching all users.');
-                }
-            });
-    };
-
-    const [first_name, setFirstName] = useState('');
-    const [last_name, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const changeFirstName = (event) => { setFirstName(event.target.value); };
-    const changeLastName = (event) => { setLastName(event.target.value); };
-    const changeEmail = (event) => { setEmail(event.target.value); };
-    const changePassword = (event) => { setPassword(event.target.value); };
-
-    const addUser = (event) => {
-        event.preventDefault();
-        // Check if the email already exists
-        if (users.some(user => user.email === email)) {
-            setError('Email already exists');
-            return;
+  const fetchUsers = () => {
+    axios
+      .get(USERS_ENDPOINT)
+      .then((response) => {
+        const usersObject = response.data.DATA;
+        const usersArray = Object.values(usersObject);
+        setUsers(usersArray);
+      })
+      .catch((error) => {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.message
+        ) {
+          setError(error.response.data.message);
+        } else {
+          setError("There was a problem fetching all users.");
         }
+      });
+  };
 
-        axios.post(USERS_ENDPOINT, { "first name": first_name, "last name": last_name, email: email, password: password })
-            .then(() => {
-                setError('');
-                setSuccessMessage('Account added, return to login');
-                fetchUsers();
-            })
-            .catch((e) => {
-                if (e.response && e.response.data && e.response.data.message) {
-                    setError(e.response.data.message);
-                } else {
-                    setError('There was a problem adding a user');
-                }
-            });
-    };
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
+  const changeFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+  const changeLastName = (event) => {
+    setLastName(event.target.value);
+  };
+  const changeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const changePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const addUser = (event) => {
+    event.preventDefault();
+    // Check if the email already exists
+    if (users.some((user) => user.email === email)) {
+      setError("Email already exists");
+      return;
+    }
+
+    axios
+      .post(USERS_ENDPOINT, {
+        "first name": first_name,
+        "last name": last_name,
+        email: email,
+        password: password,
+      })
+      .then(() => {
+        setError("");
+        setSuccessMessage("Account added, return to login");
+        fetchUsers();
+      })
+      .catch((e) => {
+        if (e.response && e.response.data && e.response.data.message) {
+          setError(e.response.data.message);
+        } else {
+          setError("There was a problem adding a user");
+        }
+      });
+  };
+
+  return (
+    <div class="backacc">
+      <div class="main">
+        <h2 class="login">Create Account</h2>
         <div className="wrapper">
-            {error && (
-                <div className="error-message">{error}</div>
-            )}
+          {error && <div className="error-message">{error}</div>}
 
-            {successMessage && (
-                <div className="success-message">{successMessage}</div>
-            )}
+          {successMessage && (
+            <div className="success-message">{successMessage}</div>
+          )}
+          <form onSubmit={addUser}>
+            <div class="first">
+              <label htmlFor="first_name">First Name</label>
+              <input
+                type="text"
+                id="first_name"
+                value={first_name}
+                onChange={changeFirstName}
+                required
+              />
+            </div>
 
-            <form onSubmit={addUser}>
-                <label htmlFor="first_name">First Name</label>
-                <input type="text" id="first_name" value={first_name} onChange={changeFirstName} required />
+            <div class="last">
+              <label htmlFor="last_name">Last Name</label>
+              <input
+                type="text"
+                id="last_name"
+                value={last_name}
+                onChange={changeLastName}
+                required
+              />
+            </div>
 
-                <label htmlFor="last_name">Last Name</label>
-                <input type="text" id="last_name" value={last_name} onChange={changeLastName} required />
+            <div class="user">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={changeEmail}
+                required
+              />
+            </div>
 
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" value={email} onChange={changeEmail} required />
+            <div class="pass">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={changePassword}
+                required
+              />
+            </div>
 
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" value={password} onChange={changePassword} required />
-
-                <button type="submit">Create Account</button>
-            </form>
-
-            <Link to="/">Back to Login</Link>
+            <button type="submit" class="submit">
+              Create Account
+            </button>
+          </form>
+          <div class="account">
+            <Link to="/" class="am">
+              Back to Login
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CreateAccount;
